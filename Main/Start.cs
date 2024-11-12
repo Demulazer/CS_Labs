e52af7150a701e046b5e199b53064191f216c1e5
123 <air42v2@gmail.com>
2024-11-12 11:25:09 +0300

    Laba 2 is done

--- Main/Start.cs
+++ Main/Start.cs
@@ -9,14 +9,6 @@ public class Start
     {
         var songView = new SongView();
         await songView.InitializeMenu();
-        //var connectionString = "Host=localhost;Port=5432;Database=mydb;Username=postgres;Password=postgres;";
-        //var databaseStorage = new DatabaseStorage(connectionString);
-
-// Пример добавления песни
-
-// Пример загрузки всех песен
-        //var songs = await databaseStorage.LoadSongsFromDatabaseAsync();
-        
 // Пример удаления песни по Id
 
     }
--- View/obj/Debug/net8.0/View.AssemblyInfo.cs
+++ View/obj/Debug/net8.0/View.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("View")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8783520ecb6d526fcce5b40d35ded7225f9b14c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+217d8aa599a459d185293d9ea5854e4b612dfa2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("View")]
 [assembly: System.Reflection.AssemblyTitleAttribute("View")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
